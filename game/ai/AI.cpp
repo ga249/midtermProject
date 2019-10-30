e048f1081e7488174994bf4c9db3917c09497fd4
Gabriel Arcanjo <ga249@njit.edu>
2019-10-30 04:01:16 -0400

    Monsters drop health, all melee weapons done, fireball and lightning spells as done as they'll ever be

--- game/ai/AI.cpp
+++ game/ai/AI.cpp
@@ -3684,20 +3684,20 @@ void idAI::OnDeath( void ){
 
 	ExecScriptFunction( funcs.death );
 
-/* DONT DROP ANYTHING FOR NOW
+// DONT DROP ANYTHING FOR NOW
 	float rVal = gameLocal.random.RandomInt( 100 );
 
 	if( spawnArgs.GetFloat( "no_drops" ) >= 1.0 ){
 		spawnArgs.Set( "def_dropsItem1", "" );
 	}else{
 		// Fixme!  Better guys should drop better stuffs!  Make drops related to guy type?  Do something cooler here?
-		if( rVal < 25 ){	// Half of guys drop nothing?
+		if( rVal > 100 ){	// Half of guys drop nothing?
 			spawnArgs.Set( "def_dropsItem1", "" );
-		}else if( rVal < 50 ){
+		}else if( rVal < 100 ){
 			spawnArgs.Set( "def_dropsItem1", "item_health_small" );
 		}
 	}
-*/
+
 }
 
 /*
